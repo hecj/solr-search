80c2d643caacae692d34e21c5b5abdafc3ff0f03
hechaojie <275070023@qq.com>
2015-02-10 17:43:20 +0800

    commit project

--- WebRoot/admin/js/extends/jQueryExtends.js
+++ WebRoot/admin/js/extends/jQueryExtends.js
@@ -5,7 +5,7 @@ $.ajaxSetup({
     contentType : "application/x-www-form-urlencoded;charset=utf-8",  
     complete : function(XMLHttpRequest, textStatus) {  
         if (XMLHttpRequest.status == 999) {  
-        	top.location.href='http://localhost:8080/solr-search/admin/index.html';
+        	top.location.href='http://localhost:8080/solr-search/admin';
             return;  
         }  
     }

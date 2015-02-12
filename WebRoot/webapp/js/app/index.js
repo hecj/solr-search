cc4fd6666337b26ae534da5fe32052a82f5b2acc
hecj <275070023@qq.com>
2015-02-13 00:36:36 +0800

    commit project

--- WebRoot/webapp/js/app/index.js
+++ WebRoot/webapp/js/app/index.js
@@ -1,20 +1,33 @@
 $(function(){
 	
 	//绑定事件
-	$('#login').bind('click',app.submit);
-	$('#reset').bind('click',app.reset);
-	$('#formLogin input[name=usercode]').val('hecj');
-	$('#formLogin input[name=password]').val('hecj');
+	//$('#login').bind('click',app.submit);
+	//$('#reset').bind('click',app.reset);
+	//$('#formLogin input[name=usercode]').val('hecj');
+	//$('#formLogin input[name=password]').val('hecj');
 	//初始化主页面
-	$(document).on("pageinit","#page_main",app.initPageMain);
+	//$(document).on("pageinit","#page_main",app.initPageMain);
 	
+});
+
+/**
+ * 页面加载事件
+ */
+$(document).on("pagebeforecreate", function(event) {
+	
+});
+$(document).on("pagecreate", function(event) {
+
+});
+$(document).on("pageinit", function(event) {
+
 });
 /**
  * webApp
  */
 var app = app || {};
 app = {
-	basePath :'http://localhost:8080/solr-search/',
+	basePath :'http://192.168.1.101:8080/solr-search/',
 	user:{},
 	submit: function(){
 		$.ajax({
@@ -25,6 +38,10 @@ app = {
 			success: function(data){
 			     if(data.code == '0'){
 			    	 app.user = data.message;
+			    	 var userStr = JSON.stringify(app.user);
+			    	 $.cookie("user", userStr, {
+		                    expires:1
+		             });
 			    	 $('#login_success').click();
 			     }else{
 			    	 $('#loginMessage').text(data.message);
@@ -43,6 +60,10 @@ app = {
 		$('#formLogin')[0].reset();
 	},
 	initPageMain: function(){
-		$('#show_usercode').text(app.user.usercode);
+		var userStr = $.cookie("user");
+		if(userStr){
+			var user = JSON.parse(userStr);
+			$('#show_usercode').text(user.username);
+		}
 	}
 }

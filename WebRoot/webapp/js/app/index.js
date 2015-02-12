614a3a0bb67ece53a83f8dc6d42b4cf72056cb87
hechaojie <275070023@qq.com>
2015-02-12 17:42:21 +0800

    commit project

--- WebRoot/webapp/js/app/index.js
+++ WebRoot/webapp/js/app/index.js
@@ -5,11 +5,17 @@ $(function(){
 	$('#reset').bind('click',app.reset);
 	$('#formLogin input[name=usercode]').val('hecj');
 	$('#formLogin input[name=password]').val('hecj');
+	//初始化主页面
+	$(document).on("pageinit","#page_main",app.initPageMain);
 	
 });
+/**
+ * webApp
+ */
 var app = app || {};
 app = {
 	basePath :'http://localhost:8080/solr-search/',
+	user:{},
 	submit: function(){
 		$.ajax({
 			type: 'POST',
@@ -18,7 +24,7 @@ app = {
 			dataType: 'json',
 			success: function(data){
 			     if(data.code == '0'){
-			    	 //$.mobile.changePage("#page_main", {reverse:false}); 
+			    	 app.user = data.message;
 			    	 $('#login_success').click();
 			     }else{
 			    	 $('#loginMessage').text(data.message);
@@ -35,5 +41,8 @@ app = {
 	},
 	reset: function(){
 		$('#formLogin')[0].reset();
+	},
+	initPageMain: function(){
+		$('#show_usercode').text(app.user.usercode);
 	}
 }

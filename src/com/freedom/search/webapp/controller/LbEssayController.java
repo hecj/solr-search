50ae2ca1f4412d530283491c6bb139c986cc5500
hechaojie <275070023@qq.com>
2015-02-13 17:42:39 +0800

    commit project

---
+++ src/com/freedom/search/webapp/controller/LbEssayController.java
@@ -0,0 +1,17 @@
+package com.freedom.search.webapp.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.freedom.search.web.controller.base.BaseController;
+
+
+@Controller
+@RequestMapping("webapp/essay/essay.htm")
+public class LbEssayController extends BaseController {
+	
+	
+	
+}

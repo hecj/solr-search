f24aae331d6ac5d021b2d120c1544e22f03f6784
hechaojie <275070023@qq.com>
2015-02-13 17:55:01 +0800

    commit project

--- src/com/freedom/search/webapp/services/LbEssayService.java
+++ src/com/freedom/search/webapp/services/LbEssayService.java
@@ -1,5 +1,12 @@
 package com.freedom.search.webapp.services;
 
-public interface LbEssayService {
+import com.freedom.search.webapp.entity.LbEssay;
 
+public interface LbEssayService {
+	
+	public boolean addLbEssay(LbEssay lbEssay);
+	
+	public boolean delLbEssay(String id);
+	
+	public boolean editLbEssay(LbEssay lbEssay);
 }
--- src/com/freedom/search/webapp/services/imp/LbEssayServiceImp.java
+++ src/com/freedom/search/webapp/services/imp/LbEssayServiceImp.java
@@ -4,7 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.freedom.search.util.StringUtil;
 import com.freedom.search.webapp.dao.LbEssayDAO;
+import com.freedom.search.webapp.entity.LbEssay;
 import com.freedom.search.webapp.services.LbEssayService;
 
 @Service("lbEssayService")
@@ -15,6 +17,25 @@ public class LbEssayServiceImp implements LbEssayService {
 
 	public void setLbEssayDAO(LbEssayDAO lbEssayDAO) {
 		this.lbEssayDAO = lbEssayDAO;
+	}
+
+	@Override
+	public boolean addLbEssay(LbEssay lbEssay) {
+		if(!StringUtil.isObjectNull(lbEssayDAO.save(lbEssay))){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delLbEssay(String id) {
+		return lbEssayDAO.delete(lbEssayDAO.findById(id));
+	}
+
+	@Override
+	public boolean editLbEssay(LbEssay lbEssay) {
+		lbEssayDAO.merge(lbEssay);
+		return true;
 	}
 	
 	

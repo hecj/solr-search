5ff83efcc47e4e457e0181717282640fe58ea5d6
hechaojie <275070023@qq.com>
2015-02-13 17:50:53 +0800

    commit project

--- src/com/freedom/search/webapp/controller/LbEssayController.java
+++ src/com/freedom/search/webapp/controller/LbEssayController.java
@@ -1,16 +1,24 @@
 package com.freedom.search.webapp.controller;
 
-
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.freedom.search.web.controller.base.BaseController;
+import com.freedom.search.webapp.services.LbEssayService;
 
 
 @Controller
 @RequestMapping("webapp/essay/essay.htm")
 public class LbEssayController extends BaseController {
+	
+	@Resource
+	private LbEssayService essayService;
+
+	public void setEssayService(LbEssayService essayService) {
+		this.essayService = essayService;
+	}
 	
 	
 	
